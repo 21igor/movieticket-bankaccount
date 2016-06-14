6e702888d04e4df1dc73d3a2a8e80c316208b811
Igor Seleznov and Ryan Streur <ryan@>
2016-06-14 15:11:32 -0700

    Add bank account project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var transactionArray = [];
+
 function Ticket(movieVal, ageVal, timeVal){
   this.movieVal = movieVal;
   this.ageVal = ageVal;
@@ -6,6 +8,20 @@ function Ticket(movieVal, ageVal, timeVal){
 
 Ticket.prototype.price = function(){
   return 3 + this.movieVal + this.ageVal + this.timeVal;
+}
+
+function Transaction(name, initialDeposit) {
+  this.transName = name;
+  this.transAmount = initialDeposit;
+  transactionArray.push(this);
+}
+
+balance = function () {
+  var balance = 0;
+  for(i=0; i<transactionArray.length; i++){
+    balance += transactionArray[i].transAmount;
+  }
+  return balance;
 }
 $(document).ready(function(){
 
@@ -19,5 +35,27 @@ $(document).ready(function(){
         var newTicket = new Ticket(movieVal, ageVal, timeVal);
         $(".displayMovie").append("<p class='ticketPrice'>$"+newTicket.price()+"</p>")
       });
+    });
+
+
+    $(".initialDepositForm").submit(function(event) {
+      event.preventDefault();
+      $(".removeAfter").remove();
+      var name = $("#initialDepositNameIn").val();
+      var initialDeposit = parseInt($("#initialDepositAmountIn").val());
+      var transaction = new Transaction(name, initialDeposit);
+      $(".balance").append("<span class='removeAfter'> $"+balance()+"</span>");
+      $(".hide-button").fadeOut();
+    });
+
+    $(".depositForm").submit(function(event) {
+      event.preventDefault();
+      $(".removeAfter").remove();
+      var depositAmount = parseInt($("#depositAmountIn").val());
+      var withdrawalAmount = parseInt($("#withdrawalAmountIn").val());
+      var amount = depositAmount - withdrawalAmount;
+      var name = transactionArray[0].transName;
+      var newTransaction = new Transaction(name, amount);
+      $(".balance").append("<span class='removeAfter'> $"+balance()+"</span>");
     });
 });
